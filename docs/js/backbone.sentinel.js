4d568a24f70e2725411f7ef6ffc488ea443fab0b
jimmynicol <james@fundly.com>
2014-01-09 20:38:44 -0800

    Upgraded the list method and refreshed the docs

--- docs/js/backbone.sentinel.js
+++ docs/js/backbone.sentinel.js
@@ -149,14 +149,20 @@
   
   // List all the components and their applicable routes
   Sentinel.list = function(){
-    var sentinel = Sentinel.getInstance();
+    var list = [],
+        sentinel = Sentinel.getInstance();
   
-    return _.map(sentinel.components, function(c){
-      return {
-        routes: c.routes,
-        name: c._name
-      };
+    _.each(sentinel.components, function(c){
+      _.each(c.routes, function(method, route){
+        list.push({ Component: c._name, Route: route, Method: method });
+      });
     });
+  
+    if (console.table){
+      console.table(list);
+    } else {
+      return list;
+    }
   };
   
   var SentinelMethods = {
@@ -349,45 +355,16 @@
     SentinelMethods
   );
   
+  function methodNotImplemented(method){
+    throw new Error('Please implement the "' + method + '" method!');
+  }
+  
   // Sentinel Component
   // ------------------
   
   // This class is a simple extension of the Backbone.View and should be used
   // in roughly the same manner. The key exception is that you can nominate from
   // within the class what routes it should respond to.
-  function methodNotImplemented(method){
-    throw new Error('Please implement the "' + method + '" method!');
-  }
-  
-  // var ComponentMethods = _.extend(Log, {
-  //   // Override `_name` in the base class to help with logging, it will prefix
-  //   // all log messages
-  //   _name: 'Component',
-  
-  //   // Set this in the component if any of the above defaults need to be
-  //   // overriden
-  //   sentinelOptions: {},
-  
-  //   // Attach a reference to Sentinel
-  //   sentinel: Sentinel.getInstance(),
-  
-  //   // Override the constructor to merge in the sentinel specific options
-  //   constructor: function(){
-  //     var _so = {};
-  
-  //     // Merge together the default options with the component specific ones
-  //     _.extend(_so, Sentinel.componentOptions, this.sentinelOptions);
-  //     this.sentinelOptions = _so;
-  
-  //     // Run the parent Backbone.View constructor
-  //     Backbone.View.apply(this, arguments);
-  //   },
-  
-  //   render: function(){ methodNotImplemented('render'); }
-  // });
-  
-  // var Component = Sentinel.Component = Backbone.View.extend(ComponentMethods);
-  
   var Component = Sentinel.Component = function(){
     var _so = {};
   
